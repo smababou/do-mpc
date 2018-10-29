395f7aef80909fe60f3ed9446eb3063291ebc369
benjacc <benjamin.karg@tu-berlin.de>
2018-10-29 11:47:23 +0100

    empty file deleted

--- examples/kite/evaluate/plot_training_data.py
+++
@@ -1,9 +0,0 @@
-# load needed modules
-import numpy as NP
-import matplotlib.pyplot as plt
-import scipy.io as sio
-
-# parameters
-path_to_data = '../data/2_uncertainties/data_batch_'
-offset = 0
-n_batches = 3
