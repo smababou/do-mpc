4da1f68bf996ce9183d69e0a301ce82d7b7e7cd9
benjacc <benjamin.karg@tu-berlin.de>
2018-11-08 15:42:50 +0100

    included osqp as projection method; 1) ipopt, 2) osqp

--- code/projection_do_mpc.py
+++ code/projection_do_mpc.py
@@ -1,6 +1,8 @@
 import numpy as NP
 from casadi import *
 from casadi.tools import *
+import osqp
+import scipy.sparse as sparse
 import pdb
 
 class projector:
@@ -8,8 +10,9 @@ class projector:
         # parameters
         self.flaaaag = False
         L = 400.0
+        self.use_osqp = True
         ub = 100.0
-        self.ems = 20 # euler mean steps
+        self.ems = 4 # euler mean steps
         self.delta_t = conf.optimizer.t_step
 
         # get symbolics
@@ -41,16 +44,6 @@ class projector:
             x_pred_old = x_pred_new
         self.x_pred = Function("x_pred",[x_sym,u_sym,p_sym],[x_pred_new])
 
-        # solver options
-        opts = {}
-        opts["expand"] = True
-        opts["ipopt.linear_solver"] = 'ma27'
-        opts["ipopt.max_iter"] = 1000
-        opts["ipopt.ma27_la_init_factor"] = 50.0
-        opts["ipopt.ma27_liw_init_factor"] = 50.0
-        opts["ipopt.ma27_meminc_factor"] = 10.0
-        opts["ipopt.tol"] = 1e-6
-
         ### Build optimization problem for projection
         # simplified system dynamics
         self.A_tilde = Function("A_tilde",[x_sym,u_sym,p_sym],[jacobian(x_eul_m,x_sym)])
@@ -73,15 +66,6 @@ class projector:
         u_hat = SX.sym("u_hat",nu,1)
         eps = SX.sym("eps",4,1)
 
-        # objective
-        # x0 = x_eul_fun(XK,u_hat,PK)
-        # A_tilde_k = A_tilde(XK,u_hat,PK)
-        # B_tilde_k = B_tilde(XK,u_hat,PK)
-        # x_new = x0 + mtimes(A_tilde_k,XK) + mtimes(B_tilde_k,u_hat-UK)
-        # x_new = self.x_pred(XK,u_hat,PK)
-        # x_new = x_eul_fun(XK,u_hat,PK)
-        # x_new = self.x_pred(XK,u_hat,PK) # NOTE: iterative euler
-
         for i in range(self.ems):
             if i == 0:
                 x_mean = x_sym + self.delta_t/self.ems * rhs_fun(x_sym,u_sym,p_sym) + mtimes(self.B_tilde(x_sym,u_sym,p_sym),u_hat-u_sym)
@@ -99,66 +83,85 @@ class projector:
         x_pred_3 = self.x_pred_lin(XK,UK,NP.array([6.0,7.0]),UK)
         x_pred_4 = self.x_pred_lin(XK,UK,NP.array([6.0,13.0]),UK)
 
-        # objective
-        # J = 1e0*(UK - u_hat)**2 + 1e4*(100.0 - self.con_fun(x_new))**2
-        w_soft = 1e3
-        J = (UK - u_hat)**2 + w_soft*(eps[0,0])**2 + w_soft*(eps[1,0])**2 + w_soft*(eps[2,0])**2 + w_soft*(eps[3,0])**2
+        if self.use_osqp:
 
-        # constraints
-        # h1 = self.con_fun(x_new_1)
-        # h2 = self.con_fun(x_new_2)
-        # h3 = self.con_fun(x_new_3)
-        # h4 = self.con_fun(x_new_4)
+            self.solver = osqp.OSQP()
+            self.H = Function("H",[x_sym],[jacobian(con,x_sym)])
+            w_soft = 1e3
+            self.P = sparse.csc_matrix(NP.diag(NP.array([1.0,w_soft,w_soft,w_soft,w_soft])))
+            self.q = NP.array([0.0,0.0,0.0,0.0,0.0])
 
-        h1 = self.con_lin_fun(x_new_1, x_pred_1)
-        h2 = self.con_lin_fun(x_new_2, x_pred_2)
-        h3 = self.con_lin_fun(x_new_3, x_pred_3)
-        h4 = self.con_lin_fun(x_new_4, x_pred_4)
+        else:
 
-        g = []
-        g.append(x_new_1)
-        g.append(x_new_2)
-        g.append(x_new_3)
-        g.append(x_new_4)
-        g.append(h1+eps[0,0])
-        g.append(h2+eps[1,0])
-        g.append(h3+eps[2,0])
-        g.append(h4+eps[3,0])
-        g.append(eps[0,0])
-        g.append(eps[1,0])
-        g.append(eps[2,0])
-        g.append(eps[3,0])
-        g = vertcat(*g)
+            # objective
+            w_soft = 1e3
+            J = (UK - u_hat)**2 + w_soft*(eps[0,0])**2 + w_soft*(eps[1,0])**2 + w_soft*(eps[2,0])**2 + w_soft*(eps[3,0])**2
 
-        lbg = []
-        lbg.append(NP.array([0.0,-0.5*pi,-1.0*pi]))
-        lbg.append(NP.array([0.0,-0.5*pi,-1.0*pi]))
-        lbg.append(NP.array([0.0,-0.5*pi,-1.0*pi]))
-        lbg.append(NP.array([0.0,-0.5*pi,-1.0*pi]))
-        lbg.append(100.0)
-        lbg.append(100.0)
-        lbg.append(100.0)
-        lbg.append(100.0)
-        lbg.append(NP.ones([4,1])*0.0)
-        self.lbg = vertcat(*lbg)
+            # constraints
+            # h1 = self.con_fun(x_new_1)
+            # h2 = self.con_fun(x_new_2)
+            # h3 = self.con_fun(x_new_3)
+            # h4 = self.con_fun(x_new_4)
 
-        ubg = []
-        ubg.append(NP.array([0.5*pi,0.5*pi,1.0*pi]))
-        ubg.append(NP.array([0.5*pi,0.5*pi,1.0*pi]))
-        ubg.append(NP.array([0.5*pi,0.5*pi,1.0*pi]))
-        ubg.append(NP.array([0.5*pi,0.5*pi,1.0*pi]))
-        ubg.append(inf)
-        ubg.append(inf)
-        ubg.append(inf)
-        ubg.append(inf)
-        ubg.append(NP.ones([4,1])*inf)
-        self.ubg = vertcat(*ubg)
+            h1 = self.con_lin_fun(x_new_1, x_pred_1)
+            h2 = self.con_lin_fun(x_new_2, x_pred_2)
+            h3 = self.con_lin_fun(x_new_3, x_pred_3)
+            h4 = self.con_lin_fun(x_new_4, x_pred_4)
 
-        # bliblablubb
-        nlp_fcn = {'f': J, 'x': vertcat(u_hat,eps), 'p': param_proj, 'g': g}
+            g = []
+            g.append(x_new_1)
+            g.append(x_new_2)
+            g.append(x_new_3)
+            g.append(x_new_4)
+            g.append(h1+eps[0,0])
+            g.append(h2+eps[1,0])
+            g.append(h3+eps[2,0])
+            g.append(h4+eps[3,0])
+            g.append(eps[0,0])
+            g.append(eps[1,0])
+            g.append(eps[2,0])
+            g.append(eps[3,0])
+            g = vertcat(*g)
 
-        # setup solver
-        self.solver = nlpsol("solver", 'ipopt', nlp_fcn, opts)
+            lbg = []
+            lbg.append(NP.array([0.0,-0.5*pi,-1.0*pi]))
+            lbg.append(NP.array([0.0,-0.5*pi,-1.0*pi]))
+            lbg.append(NP.array([0.0,-0.5*pi,-1.0*pi]))
+            lbg.append(NP.array([0.0,-0.5*pi,-1.0*pi]))
+            lbg.append(100.0)
+            lbg.append(100.0)
+            lbg.append(100.0)
+            lbg.append(100.0)
+            lbg.append(NP.ones([4,1])*0.0)
+            self.lbg = vertcat(*lbg)
+
+            ubg = []
+            ubg.append(NP.array([0.5*pi,0.5*pi,1.0*pi]))
+            ubg.append(NP.array([0.5*pi,0.5*pi,1.0*pi]))
+            ubg.append(NP.array([0.5*pi,0.5*pi,1.0*pi]))
+            ubg.append(NP.array([0.5*pi,0.5*pi,1.0*pi]))
+            ubg.append(inf)
+            ubg.append(inf)
+            ubg.append(inf)
+            ubg.append(inf)
+            ubg.append(NP.ones([4,1])*inf)
+            self.ubg = vertcat(*ubg)
+
+            # bliblablubb
+            nlp_fcn = {'f': J, 'x': vertcat(u_hat,eps), 'p': param_proj, 'g': g}
+
+            # solver options
+            opts = {}
+            opts["expand"] = True
+            opts["ipopt.linear_solver"] = 'ma27'
+            opts["ipopt.max_iter"] = 1000
+            opts["ipopt.ma27_la_init_factor"] = 50.0
+            opts["ipopt.ma27_liw_init_factor"] = 50.0
+            opts["ipopt.ma27_meminc_factor"] = 10.0
+            opts["ipopt.tol"] = 1e-6
+
+            # setup solver
+            self.solver = nlpsol("solver", 'ipopt', nlp_fcn, opts)
 
 def make_step_projection(conf):
 
@@ -183,10 +186,15 @@ def make_step_projection(conf):
     # pk = NP.copy(conf.observer.ekf.x_hat[nx:])
 
     # predict state
-    xp1 = proj.x_pred(xk,uk,NP.array([4.0,7.0]))
-    xp2 = proj.x_pred(xk,uk,NP.array([4.0,13.0]))
-    xp3 = proj.x_pred(xk,uk,NP.array([6.0,7.0]))
-    xp4 = proj.x_pred(xk,uk,NP.array([6.0,13.0]))
+    p1 = NP.array([4.0,7.0])
+    p2 = NP.array([4.0,13.0])
+    p3 = NP.array([6.0,7.0])
+    p4 = NP.array([6.0,13.0])
+    xp1 = proj.x_pred(xk,uk,p1)
+    xp2 = proj.x_pred(xk,uk,p2)
+    xp3 = proj.x_pred(xk,uk,p3)
+    xp4 = proj.x_pred(xk,uk,p4)
+    xp_vert = NP.vstack([xp1,xp2,xp3,xp4])
 
     con1 = proj.con_fun(xp1)
     con2 = proj.con_fun(xp2)
@@ -194,72 +202,77 @@ def make_step_projection(conf):
     con4 = proj.con_fun(xp4)
 
     if  (con1<100.0) or (con2<100.0) or (con3<100.0) or (con4<100.0):
-        param_k = proj.param(0)
-        param_k["uk"] = uk
-        param_k["xk"] = xk
-        param_k["pk"] = pk
-        result = proj.solver(x0=vertcat(uk,NP.ones([4,1])), lbx=-10.0, ubx=10.0, lbg=proj.lbg, ubg=proj.ubg, p=param_k)
-        u_opt = result["x"]
-        # pdb.set_trace()
-        # proj.flaaaag = True
-        conf.optimizer.u_mpc = NP.reshape(u_opt[0],(1,-1))
 
-        # conf.make_step_simulator()
-        # print(conf.simulator.xf_sim)
-        # print(proj.x_pred_lin(xk,uk,pk,u_opt))
-        # print(proj.x_pred(xk,u_opt,pk))
-        # pdb.set_trace()
+        if proj.use_osqp:
 
-    # # linearized system
-    # x_eul = x_sym + delta_t * rhs
-    # x_eul_fun = Function("x_eul_fun",[x_sym,u_sym,p_sym],[x_eul])
-    # B_tilda = jacobian(x_eul,u_sym)
-    # B_tilda_fun = Function("B_tilda",[x_sym,u_sym,p_sym],[B_tilda])
-    # B_tilda = substitute(B_tilda,vertcat(x_sym,u_sym,p_sym),vertcat(xk,uk,pk))
-    #
-    # # Linearize system #NOTE: if robust -> several linear systems
-    # dx = Function('dx',[vertcat(x_sym,u_sym,p_sym)],[rhs])#(rhs,vertcat(x_sym,u_sym,p_sym),vertcat(xk,uk,pk))
-    # # Predict states
-    #
-    # # cons fun
-    # con = -conf.model.ocp.cons[0]
-    # con_fun = Function('con_fun',[x_sym],[con])
-    # con_dx = jacobian(con,x_sym)
-    #
-    # if (con_fun(xp) <= -conf.model.ocp.cons_ub):
-    #     pdb.set_trace()
-    #     # linearize constraint xp
-    #     u_hat = SX.sym("u_hat")
-    #     du = mtimes(B_tilda,(u_hat-uk))
-    #     h = con + mtimes(con_dx,du)
-    #     h = substitute(h,p_sym,pk)
-    #     h = substitute(h,x_sym,xk)
-    #     dx_lin = jacobian(rhs,vertcat(x_sym,u_sym))
-    #     dx_lin = substitute(dx_lin,p_sym,pk)
-    #
-    #     # lhs_p =
-    #
-    #     con_lin_fun = Function('con_lin_fun',[x_sym],[jacobian(-conf.model.ocp.cons[0],x_sym)])
-    #     con_lin_xp = con_lin_fun(xp)
-    #
-    #     # variables and paramters
-    #     u_hat = MX.sym("u_hat",nu,1)
-    #
+            # compute A
+            H_1 = proj.H(xp1)
+            H_2 = proj.H(xp2)
+            H_3 = proj.H(xp3)
+            H_4 = proj.H(xp4)
+            Bt_1 = proj.B_tilde(xp1,uk,p1)
+            Bt_2 = proj.B_tilde(xp2,uk,p2)
+            Bt_3 = proj.B_tilde(xp3,uk,p3)
+            Bt_4 = proj.B_tilde(xp4,uk,p4)
+            Bt_vert = NP.vstack([Bt_1,Bt_2,Bt_3,Bt_4])
+            Ht_1 = mtimes(H_1,Bt_1)
+            Ht_2 = mtimes(H_2,Bt_2)
+            Ht_3 = mtimes(H_3,Bt_3)
+            Ht_4 = mtimes(H_4,Bt_4)
+            A_ur = NP.zeros([12,4])
+            A_ul = NP.vstack([Bt_1,Bt_2,Bt_3,Bt_4])
+            A_u = NP.hstack([A_ul,A_ur])
+            A_mr = NP.diag(NP.ones(4))
+            A_ml = NP.vstack([Ht_1,Ht_2,Ht_3,Ht_4])
+            A_m = NP.hstack([A_ml,A_mr])
+            A_l = NP.diag(NP.ones(5))
+            A = sparse.csc_matrix(NP.vstack([A_u,A_m,A_l]))
 
-    #
-    #
-    #     xn = x_eul_fun(XK,u_hat,PK)
-    #
-    #     # objective
-    #     J = (UK - u_hat)**2
-    #
+            # update q
+            proj.q[0] = -uk
+
+            # compute bounds
+            l = NP.reshape(NP.array([0.0,-0.5*pi,-1.0*pi,0.0,-0.5*pi,-1.0*pi,0.0,-0.5*pi,-1.0*pi,0.0,-0.5*pi,-1.0*pi,100.0,100.0,100.0,100.0,-10.0,0.0,0.0,0.0,0.0]),(-1,1))
+            u = NP.reshape(NP.array([0.5*pi,0.5*pi,1.0*pi,0.5*pi,0.5*pi,1.0*pi,0.5*pi,0.5*pi,1.0*pi,0.5*pi,0.5*pi,1.0*pi,500.0,500.0,500.0,500.0,10.0,1000.0,1000.0,1000.0,1000.0]),(-1,1))
+
+            for i in range(4):
+                l[i*nx:(i+1)*nx] = l[i*nx:(i+1)*nx] - xp_vert[i*nx:(i+1)*nx] + mtimes(Bt_vert[i*nx:(i+1)*nx],uk)
+                u[i*nx:(i+1)*nx] = u[i*nx:(i+1)*nx] - xp_vert[i*nx:(i+1)*nx] + mtimes(Bt_vert[i*nx:(i+1)*nx],uk)
+
+            l[12] = l[12] - con1 + mtimes(H_1,xp1)
+            l[13] = l[13] - con2 + mtimes(H_2,xp2)
+            l[14] = l[14] - con3 + mtimes(H_3,xp3)
+            l[15] = l[15] - con4 + mtimes(H_4,xp4)
+
+            u[12] = u[12] - con1 + mtimes(H_1,xp1)
+            u[13] = u[13] - con2 + mtimes(H_2,xp2)
+            u[14] = u[14] - con3 + mtimes(H_3,xp3)
+            u[15] = u[15] - con4 + mtimes(H_4,xp4)
+
+            # setup solver
+            proj.solver.setup(proj.P, proj.q, A, NP.squeeze(l), NP.squeeze(u), scaling = False)
+
+            # solve problem
+            res = proj.solver.solve()
+            conf.optimizer.u_mpc = res.x[0]
+
+        else:
+
+            param_k = proj.param(0)
+            param_k["uk"] = uk
+            param_k["xk"] = xk
+            param_k["pk"] = pk
+            result = proj.solver(x0=vertcat(uk,NP.ones([4,1])), lbx=-10.0, ubx=10.0, lbg=proj.lbg, ubg=proj.ubg, p=param_k)
+            u_opt = result["x"]
+            conf.optimizer.u_mpc = NP.reshape(u_opt[0],(1,-1))
 
 
-    #
-    #     # get solution
-    #     param_true = param_proj(0)
-    #     param_true["uk"] = uk
-    #     param_true["xk"] = xk
-    #     param_true["pk"] = pk
-    #     result = solver(x0=uk, lbx=-1000.0, ubx=1000.0, lbg=lbg, ubg=ubg, p=param_true)
-    #     u_opt = result["x"][:nu]
+        # H[0] = (cos(x_1)*(400*cos(x_0)))
+        # H[1] = (-((400*sin(x_0))*sin(x_1)))
+        # H[2] = 0.0
+
+        # Bt[0,0] = @1=0.028, @2=(E_0-(@1*sq(u_tilde))), @3=(tan(theta)/@2), @4=cos(theta), @5=(@1*(u_tilde+u_tilde)), (-(0.075*(((cos(psi)-@3)*(0.0025*(@4*(v_0*@5))))+((((v_0*@2)*@4)/400)*((@3/@2)*@5)))))
+        #
+        # Bt[1,0] = (0.075*(sin(psi)*((cos(theta)*(v_0*(0.028*(u_tilde+u_tilde))))/(400*sin(theta)))))
+        #
+        # Bt[2,0] = @1=0.028, @2=cos(theta), @3=400, @4=(@2*(v_0*(@1*(u_tilde+u_tilde)))), (0.075*(((((v_0*(E_0-(@1*sq(u_tilde))))*@2)/@3)-(u_tilde*(0.0025*@4)))+(cos(theta)*(sin(psi)*(@4/(@3*sin(theta)))))))
